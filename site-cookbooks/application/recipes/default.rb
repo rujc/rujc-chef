16166b389e52b93266c8509d1b8da4bb4b14f9dc
Max Prokopiev <me@maxprokopiev.com>
2014-04-05 23:01:44 +0400

    Add postgresql recipe

--- site-cookbooks/application/recipes/default.rb
+++ site-cookbooks/application/recipes/default.rb
@@ -119,6 +119,37 @@ template "/etc/init.d/unicorn_#{application_name}" do
   variables template_variables
 end
 
+# Database
+
+include_recipe 'postgresql::client'
+include_recipe 'postgresql::ruby'
+include_recipe 'postgresql::server'
+
+postgresql_connection_info = {
+  host: node.postgresql.config.listen_addresses,
+  port: node.postgresql.config.port,
+  username: 'postgres',
+  password: node.postgresql.password.postgres
+}
+
+postgresql_database(node.rails.application.db.name) do
+  connection postgresql_connection_info
+  action :create
+end
+
+postgresql_database_user(node.rails.application.db.username) do
+  connection postgresql_connection_info
+  password node.rails.application.db.password
+  action :create
+end
+
+postgresql_database_user(node.rails.application.db.username) do
+  connection postgresql_connection_info
+  database_name node.rails.application.db.name
+  privileges [:all]
+  action :grant
+end
+
 template database_yml_config do
   source "database.yml.erb"
 
