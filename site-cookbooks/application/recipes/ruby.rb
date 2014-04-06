fef9d73c4ab57e63ddef537721a1eb8954c430b4
Max Prokopiev <me@maxprokopiev.com>
2014-04-06 19:55:10 +0400

    Move to ruby 2.1.1

--- site-cookbooks/application/recipes/ruby.rb
+++ site-cookbooks/application/recipes/ruby.rb
@@ -2,7 +2,7 @@
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
-rbenv_ruby "2.0.0-p451"
+rbenv_ruby "2.1.1"
 rbenv_gem "bundler" do
-  ruby_version "2.0.0-p451"
+  ruby_version "2.1.1"
 end
